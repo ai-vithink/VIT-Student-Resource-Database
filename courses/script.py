d196a6c6acbbe566ec0ea9625a5aba9d8e30168c
kunal768 <33108756+kunal768@users.noreply.github.com>
2019-10-08 02:42:55 +0530

    Update script.py

--- courses/script.py
+++ courses/script.py
@@ -1,12 +1,13 @@
 import os
 import pandas as pd 
 
+# add new courses to the csv 
 data = pd.read_csv("courses.csv")
-path = "C:\\Users\\Kunal K.S. Sahni\\Desktop\\courses"
+
+path = "<add starting folder>"
 
 for course in data["Courses"] : 
 	course = '"' + course + '"'	
-	# print(course)
 	os.system("mkdir " + course + " && echo " + course + " > " + course+"/readme.md")
 	os.chdir(course.strip('\"'))
 	os.system("mkdir YouTube PDF DOC PPT QuestionPapers" + " && echo add links >  YouTube/YouTube.md" + " && echo add PDFS >  PDF/PDF.md " + "&& echo add docs >  DOC/DOC.md"+ " && echo add ppts >  PPT/PPT.md"+ " && echo add question papers >  QuestionPapers/QuestionPapers.md ")
